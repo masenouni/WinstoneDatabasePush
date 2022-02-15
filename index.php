7c96a8c7f333c57ef18aa0d7eeaa7c802b034d1d
winstone <winstoneothis@gmail.com>
2022-02-16 00:04:11 +0300

    update

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ if(isset($_SESSION['user_email'])){
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login - Footmarkz.com</title>
+    <title>Login-Winstone</title>
     <link rel="stylesheet" href="style.css" media="all" type="text/css">
 </head>
 
